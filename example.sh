eee8a91a1f4a098a0d3d317fcfb6cc3f267927a8
Mitsuru Nakada <m-nakada@nekojarashi.com>
2016-05-05 16:13:48 +0900

    Add example.sh

---
+++ example.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+mogif -f 0.3 -c 🚶🏃
+mogif -f 0.5 -c 🙎🙆
+mogif -f 0.5 -c 🙎🙅
+mogif -f 0.5 -c 😺😸
+mogif -f 0.5 -c 😴💤
+mogif -f 0.5 -c 😧😨
+mogif -f 0.5 -c 😈👿
+mogif -f 0.5 -c 👬👯
+mogif -f 0.5 -c 🐵🙉
+mogif -f 0.5 -c ⭐️🌟
+mogif -f 0.5 -c ❤️💔
+mogif -f 0.5 -c 🍺🍻
+mogif -f 0.3 -c 🌕🌖🌗🌘🌑🌒🌓🌔
+mogif -f 0.5 -c ありがとう
+mogif -f 0.5 -c 感謝
