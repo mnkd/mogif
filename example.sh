efa16ea9ce356e2cdc1a16baf49a79dfe04e1301
Mitsuru Nakada <m-nakada@nekojarashi.com>
2016-05-05 17:20:41 +0900

    Update example command

--- example.sh
+++ example.sh
@@ -11,6 +11,8 @@ mogif -f 0.5 -c 🐵🙉
 mogif -f 0.5 -c ⭐️🌟
 mogif -f 0.5 -c ❤️💔
 mogif -f 0.5 -c 🍺🍻
+mogif -f 0.3 -c 🔈🔉🔊
+mogif -f 0.5 -c ‼️🚨
 mogif -f 0.3 -c 🌕🌖🌗🌘🌑🌒🌓🌔
 mogif -f 0.5 -c ありがとう
 mogif -f 0.5 -c 感謝
